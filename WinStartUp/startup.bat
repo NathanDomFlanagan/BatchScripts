f88cdbb3e6cb80c3cac9b1d0d758639464cc3b7d
Nathan Flanagan <102491889+NathanDomFlanagan@users.noreply.github.com>
2024-04-22 15:27:38 +1200

    Fixing up this repository
    Added filing to add clarity

---
+++ WinStartUp/startup.bat
@@ -0,0 +1,4 @@
+@echo off
+Start "Discord" "C:\Users\Nathan\AppData\Local\Discord\app-1.0.9032\Discord.exe"
+Start "Floorp" "C:\Program Files\Ablaze Floorp\floorp.exe"
+Start "TaskbarX" "C:\Users\Nathan\Documents\Tweaks\TaskbarX_1.7.8.0_x64\TaskbarX.exe"
